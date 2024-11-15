c1f90213c2efeaff8c18510cf56709f4444ca568
Vaishali Singh <94376468+singhvaishu@users.noreply.github.com>
2024-11-16 01:21:27 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ const App = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/signup" element={<Signup />} />
+        <Route path="/" element={<Signup />} />
         <Route path="/login" element={<Login />} />
         <Route path="/cars" element={<CarList />} />
         <Route path="/cars/new" element={<CarForm />} />
